22be2cf479d69874d71d2b7e8bf9dba6584d6301
murynowicz <murynowicz@gmail.com>
2016-11-17 00:18:42 +0100

    Removed csfr from view

--- ssbot/views.py
+++ ssbot/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, StreamingHttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import json
 
 
 def index(request):
     return HttpResponse("Index url")
 
-
+@csrf_exempt
 def botendpoint(request):
     if request.method=='POST':
             received_json_data=json.loads(request.body)
