d4f559c63ad46ebe45314a2419d31782ed039fc3
murynow <murynow@softserveinc.com>
2016-11-18 16:33:19 +0100

    added get JWT token

--- ssbot/models.py
+++ ssbot/models.py
@@ -3,3 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class HTTPLoger(models.Model):
+    date = models.DateTimeField('time stamp of post')
+    httpStuff = models.TextField(blank=True, null=True)
--- ssbot/views.py
+++ ssbot/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse, StreamingHttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from .models import *
 import os
 import requests
 
 
 def index(request):
-    return HttpResponse("Index url")
+    top_10_http_requests = HTTPLoger.objects.order_by('-date')[:10]
+    output = ', '.join([http_request.httpStuff for http_request in top_10_http_requests])
+    return HttpResponse(output)
+
 
 @csrf_exempt
 def botendpoint(request):
@@ -15,6 +19,7 @@ def botendpoint(request):
             received_json_data=json.loads(request.body)
             #received_json_data=json.loads(request.body)
             return StreamingHttpResponse('POST received: ' + str(received_json_data))
-    return StreamingHttpResponse('Accepting only POST')
+    pass
+    #return StreamingHttpResponse('Accepting only POST')
 
 
