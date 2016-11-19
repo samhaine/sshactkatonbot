9180a2fd92011ed59d2aecc3ecf74c9ccbc57a36
Radosław Warzocha <radoslaw.warzocha@gmail.com>
2016-11-19 04:03:05 +0200

    Scaffolding of clean-up cronjob

---
+++ autofeed_plugin/cronjob.py
@@ -0,0 +1,10 @@
+from .models import Approver, LinkPost
+
+def get_tommorows_posts():
+	pass
+
+def get_todays_approved_posts():
+	pass
+
+if __name__ == "__main__":
+	pass
--- autofeed_plugin/models.py
+++ autofeed_plugin/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Approver(models.Model):
 	name = models.CharField()
-	to_approve = models.ForeignKey('LinkPost')
+	to_approve = models.ForeignKey('LinkPost', on_delete=models.CASCADE)
 
 class LinkPost(models.Model):
 	link = models.URLField()
