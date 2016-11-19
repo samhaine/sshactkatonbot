82eaf930ccffc3f9859d6cb0a21eb8116d3e2a7b
murynow <murynow@softserveinc.com>
2016-11-19 04:48:45 +0100

    json datetime problem fix part2

--- autofeed_plugin/models.py
+++ autofeed_plugin/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Approver(models.Model):
-	name = models.CharField()
+	name = models.CharField(max_length=400)
 	to_approve = models.ForeignKey('LinkPost')
 
 class LinkPost(models.Model):
