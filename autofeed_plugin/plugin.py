7d0fe438985493dce6aae3372395bdceef6c985d
murynow <murynow@softserveinc.com>
2016-11-19 18:23:29 +0100

    fix stuff

--- autofeed_plugin/plugin.py
+++ autofeed_plugin/plugin.py
@@ -5,46 +5,48 @@ from plugin_base.plugin import ApiAiBase, S_OK
 from .models import Approver, LinkPost
 
 class AutofeedPlugin(ApiAiBase):
-    def _read_token(self):
-        return '398d090459b444699621cc4324ccf006' #this should not be here, but you know, fuck security because Hackaton
+	def _read_token(self):
+		return '398d090459b444699621cc4324ccf006' #this should not be here, but you know, fuck security because Hackaton
 
-    def auto_feed(self):
-    	url = self._ctx['url']
-    	date = datetime.datetime.strptime(self._ctx['date'], 'mm-dd-yyyy')
-    	date = datetime.date(date.year, date.month, date.day)
-    	decisionmakers = self._ctx['any'].split('and') # this is the parameter's name in the api.ai, some problem?
+	def auto_feed(self):
+		url = self._ctx['url']
+		print "---------------------------------------------------------"
+		print self._ctx['date']
+		date = datetime.datetime.strptime(self._ctx['date'], 'yyyy-mm-dd')
+		date = datetime.date(date.year, date.month, date.day)
+		decisionmakers = self._ctx['any'].split('and') # this is the parameter's name in the api.ai, some problem?
 
-    	post = LinkPost(url, date, false)
-    	post.save()
-    	for dm in decisionmakers:
-    		apr = Approver(dm, post)
-    		apr.save()
+		post = LinkPost(url, date, false)
+		post.save()
+		for dm in decisionmakers:
+			apr = Approver(dm, post)
+			apr.save()
 
-    	return self._ctx['speech'], S_OK
+		return self._ctx['speech'], S_OK
 
-    def approve(self):
-    	user = self._ctx['name']
-    	links_ids = self._ctx['ids']
+	def approve(self):
+		user = self._ctx['name']
+		links_ids = self._ctx['ids']
 
-    	# there is obvious race-condition here. Fortunately we won't run into it on the demo :P
-    	apprvs = Approver.objects().filter(name__exact=user, to_approve__in=links_ids)
-    	for app_record in apprvs:
-    		app_record.approved = True
-    		all_apprvs = app_record.to_approve.approver_set
-    		all_apprvs = [x.approved for x in all_apprvs]
-    		if all(all_apprvs):
-    			app_record.to_approve.approved = True
-    			app_record.to_approve.publish()
-    			app_record.to_approve.delete()
-    		else:
-    			app_record.save()
+		# there is obvious race-condition here. Fortunately we won't run into it on the demo :P
+		apprvs = Approver.objects().filter(name__exact=user, to_approve__in=links_ids)
+		for app_record in apprvs:
+			app_record.approved = True
+			all_apprvs = app_record.to_approve.approver_set
+			all_apprvs = [x.approved for x in all_apprvs]
+			if all(all_apprvs):
+				app_record.to_approve.approved = True
+				app_record.to_approve.publish()
+				app_record.to_approve.delete()
+			else:
+				app_record.save()
 
-    	return self._ctx['speech'], S_OK
+		return self._ctx['speech'], S_OK
 
-    def deny(self):
-    	user = self._ctx['name']
-    	links_ids = self._ctx['ids']
+	def deny(self):
+		user = self._ctx['name']
+		links_ids = self._ctx['ids']
 
-    	denied_posts = LinkPost.objects().filter(id__in=links_ids).delete()
-    	return self._ctx['speech'], S_OK
+		denied_posts = LinkPost.objects().filter(id__in=links_ids).delete()
+		return self._ctx['speech'], S_OK
 
